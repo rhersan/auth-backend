0e6600f022e113c8de8414e01188a435453eb057
Ricardo Hernandez Santiago <ext.ricardo.hernande>
2022-08-24 16:03:42 -0500

    Despliegue

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -21,6 +22,12 @@ app.use( express.json())
 
 // Rutas
 app.use('/api/auth', require('./routes/auth') );
+
+// Otras rutas
+app.get('*', (req, resp) => {
+    resp.sendFile( path.resolve(__dirname, 'public/index.html'));
+});
+
 
 
 app.listen(process.env.PORT, () => {
